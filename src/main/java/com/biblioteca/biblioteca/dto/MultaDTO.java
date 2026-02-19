efb4f246510c01a434562cbe9505822b3f33705f
Caradophp <contatolucianofriebe@gmail.com>
2026-02-19 12:16:30 -0300

    Validação na entrada de dados referentes a multa

--- src/main/java/com/biblioteca/biblioteca/dto/MultaDTO.java
+++ src/main/java/com/biblioteca/biblioteca/dto/MultaDTO.java
@@ -1,8 +1,16 @@
 package com.biblioteca.biblioteca.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record MultaDTO(
+        @NotNull(message = "Id do emprétimo deve ser informado")
         long idEmprestimo,
+
+        @NotNull(message = "Valor da multa deve ser informado")
         float multaValor,
+
+        @NotBlank(message = "Forma de pagamento deve ser irformada")
         String tipoPagamento
 ) {
 }
