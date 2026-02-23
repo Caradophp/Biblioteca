cfdb67b9341287b2ee9967d1ea393ccf8e763701
Caradophp <contatolucianofriebe@gmail.com>
2026-02-22 21:32:43 -0300

    Validação no EmailRequest

--- src/main/java/com/biblioteca/biblioteca/request/EmailRequest.java
+++ src/main/java/com/biblioteca/biblioteca/request/EmailRequest.java
@@ -1,6 +1,9 @@
 package com.biblioteca.biblioteca.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record EmailRequest(
+        @NotBlank(message = "O e-mail deve ser informado")
         String email
 ) {
 }
